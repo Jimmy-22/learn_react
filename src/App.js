5f2dfc5f120cadc0b3f2bfce97c4211c7e792ab6
xujin <xujin@bosonspace.com>
2022-06-16 10:57:22 +0800

    feat：子传父

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React from 'react'
 
-// 函数式子组件
-function FSon(props) {
-  console.log(props)
+function Son(props) {
+  function handleClick() {
+    // 3.子组件通过props调用 callback
+    props.changeMsg('new Message')
+  }
   return (
     <div>
-      子组件1
       {props.msg}
+      <button onClick={handleClick}>change</button>
     </div>
   )
 }
 
-// 类子组件
-class CSon extends React.Component {
+class App extends React.Component {
+  state = {
+    message: 'this is message!'
+  }
+
+  // 1.提供callback-用于接收数据
+  // 4.将子组件中的数据作为参数传递给callback
+  changeMessage = (newMsg) => {
+    console.log('子组件传过来的数据', newMsg)
+    this.setState({
+      message: newMsg
+    })
+  }
+
   render() {
     return (
       <div>
-        子组件2
-        {this.props.msg}
-      </div>
-    )
-  }
-}
-
-// 父组件
-class App extends React.Component {
-  state = {
-    message: 'this is msg'
-  }
-
-  render() {
-    return (
-      <div className="App">
         <div>父组件</div>
-        <FSon msg={this.state.message} />
-        <CSon msg={this.state.message} />
+        {/* 2.将函数作为属性的值，传给子组件 */}
+        <Son msg={this.state.message} changeMsg={this.changeMessage} />
       </div>
     )
   }
